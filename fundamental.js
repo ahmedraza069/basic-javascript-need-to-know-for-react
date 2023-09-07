258876a7f2fc219554ff980cd719ab53fc3b743d
Ahmed Raza <116287441+ahmedraza069@users.noreply.github.com>
2023-09-07 15:02:24 +0600

    need to know for react

---
+++ fundamental.js
@@ -0,0 +1,128 @@
+// Need t know React
+
+// var  num = 0;
+// let numOne = 122;
+// const string = 'Hello world';
+
+// const isActive = true;
+
+// declear variable
+// let var const bolean sting number
+
+// let money = 50;
+// if (money > 40) {
+//   console.log("I eat biriany");
+// } else {
+//   console.log("amie kichu khabo na");
+// }
+// // two types of array
+// let friends = ["rakib", "sakil", "romjan", "johirul", "shanto"];
+// let numbers = [121, 23, 34, 25, 345, 66, 78];
+// for (let i = 0; i < numbers.length; i++) {
+//     const num = numbers[i];
+// }
+
+// for (const num of numbers) {
+//     console.log(num)
+// }
+
+// function add(a, b){
+//     const multiplySum = a * b
+//     return multiplySum
+// }
+
+// console.log(add(2, 4));
+
+// const phone = {
+//     name:'Samsung',
+//     model:'s20',
+//     price: 120000
+// }
+
+// console.log(phone.name);
+// console.log(phone['name']);
+
+// const name = 'Md. Rakibul Hasan Raza';
+// const age = 20;
+// const address = 'Bangladesh';
+
+// const totalLine = `My name is ${name}. I am ${age} years old. I lives in ${address}`;
+// console.log(totalLine);
+
+// const add = (a, b ) => a + b;
+// console.log(add(56,6));
+
+// const num = () => num;
+
+// const sum = (x, y, z) => x + y + z;
+
+// const multiFunc = () =>{
+//     const num = 10;
+//     const numOne = 20 ;
+//     const result = num + numOne;
+//     return result;
+// }
+// console.log(multiFunc);
+
+// const arrayOne = [33,24, 345, 356, 3, 3, 4, 24, 5, 2];
+
+// const arrayTwo = [...arrayOne, 1, 2, 3,4]
+// console.log(arrayTwo);
+
+
+// const phone = {
+//     name:'Samsung',
+//     model:'s20',
+//     price: 120000
+// }
+
+// const  newObj = {...phone, camera: '45mx'}
+
+// console.log(newObj)
+
+// const num = [1, 2, 3, 4, 5];
+// const total = 0;
+// const sum = num.map(n => total = total + n);
+// console.log(sum);
+
+
+
+// const num = [1, 2, 3, 4, 5];
+// num.forEach(n => {
+//     console.log(n)
+// })
+
+
+// const num = [1, 2, 3, 4, 5];
+
+// const [x, y] = num;
+// console.log(x, y);
+
+
+
+// const phone = {
+//     name:'Samsung',
+//     model:'s20',
+//     price: 120000
+// }
+// const key = Object.keys(phone);
+// console.log(key);
+
+// const value = Object.values(phone);
+// console.log(value);
+
+// const {name, price} = phone;
+// console.log(name, price);
+
+// const loadData = async () =>{
+//     const res = await fetch('url');
+//     const data =await res.json();
+//     console.log(data);
+// }
+
+
+const myTk = 50;
+
+const money = myTk < 10 ? 'amie coca-cola khabo' : 'amie panie khabo';
+
+console.log(money);
